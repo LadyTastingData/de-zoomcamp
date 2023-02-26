481aaeb33ba1fc442e3403c5c1053e57a1d718ac
LadyTastingData <ladytastingdata@gmail.com>
2023-02-26 00:58:32 +0000

    Edit web_to_gcs code

--- week_2/homework2/etl_web_to_gcs.py
+++ week_2/homework2/etl_web_to_gcs.py
@@ -77,15 +77,15 @@ def etl_web_to_gcs(year, month, color) -> None:
     write_gcs(path)
 
 @flow()
-def etl_parent_flow(months: list[int] = [7, 8, 9, 10, 11, 12], year: int = 2019, color: str = "yellow"):
+def etl_parent_flow(months: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], year: int = 2020, color: str = "yellow"):
     for month in months:
         etl_web_to_gcs(year, month, color)
 
 
 if __name__ == "__main__":
     color = "yellow"
-    months = [7, 8, 9, 10, 11, 12]
-    year = 2019
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    year = 2020
     etl_parent_flow(months, year, color)
 
 
